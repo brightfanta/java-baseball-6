76792e8f7e8b507f2926f18b008c21268e6cb571
brightfanta <brightfanta@gmail.com>
2023-12-03 20:33:31 +0900

    refactor: 입력 검증 메서드 숫자 클래스로 이동(Parser -> Numbers)

--- src/main/java/baseball/utility/Parser.java
+++ src/main/java/baseball/utility/Parser.java
@@ -2,21 +2,11 @@ package baseball.utility;
 
 import baseball.exception.ExceptionHandler;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Parser {
-    private static final Pattern REGEX_PATTERN = Pattern.compile("^\\d{100,999}$");
-
     private Parser() {
     }
 
     public static int parseNumberInput(String numberInput) {
         return ExceptionHandler.tryOnParseIntException(() -> Integer.parseInt(numberInput));
-    }
-
-    private static boolean matchWithRegex(String input) {
-        Matcher matcher = REGEX_PATTERN.matcher(input);
-        return !matcher.matches();
     }
 }
