8fec0dba3d89ff6c33f7be99a186a7f02c29c5ab
brightfanta <brightfanta@gmail.com>
2023-12-03 13:35:22 +0900

    feat: 예외처리 클래스 구현

---
+++ src/main/java/baseball/exception/ExceptionHandler.java
@@ -0,0 +1,30 @@
+package baseball.exception;
+
+import baseball.view.output.ErrorOutputWriter;
+
+import java.time.DateTimeException;
+import java.util.function.Supplier;
+
+public class ExceptionHandler {
+
+    public ExceptionHandler() {
+    }
+
+    public static <T> T retryOnBusinessException(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (BusinessException exception) {
+                ErrorOutputWriter.println(exception.getMessage());
+            }
+        }
+    }
+
+    public static <T> T tryOnParseIntException(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (DateTimeException | NumberFormatException exception) {
+            throw BusinessException.of(ErrorCode.INVALID_NUMBER, exception);
+        }
+    }
+}
