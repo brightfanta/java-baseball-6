77eb48d22c65d8a643691d7e61f5804ef64c9843
brightfanta <brightfanta@gmail.com>
2023-12-03 20:30:38 +0900

    feat: 게임 컨트롤러 클래스 입력 요청 및 입력 숫자 클래스 생성 메서드 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,6 +1,23 @@
 package baseball.controller;
 
+import baseball.domain.Numbers;
+import baseball.exception.ExceptionHandler;
+import baseball.view.input.InputReader;
+import baseball.view.output.GameOutputWriter;
+
+import static baseball.view.constants.ResponseMessage.START_MESSAGE;
+
 public class GameController {
     public GameController() {
+    }
+
+    public static Numbers requestNumbers() {
+        GameOutputWriter.printMessageResponse(START_MESSAGE);
+        return ExceptionHandler.retryOnBusinessException(GameController::createGameNumbersFromInput);
+    }
+
+    private static Numbers createGameNumbersFromInput() {
+        String numberInput = InputReader.readInput();
+        return Numbers.create(numberInput);
     }
 }
