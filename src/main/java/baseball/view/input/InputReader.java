8708d692aa112747889303db55fce9c70266694a
brightfanta <brightfanta@gmail.com>
2023-12-03 13:18:30 +0900

    feat: 사용자 입력 클래스 구현

---
+++ src/main/java/baseball/view/input/InputReader.java
@@ -0,0 +1,13 @@
+package baseball.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputReader {
+
+    public InputReader() {
+    }
+
+    public static String readInput() {
+        return Console.readLine();
+    }
+}
