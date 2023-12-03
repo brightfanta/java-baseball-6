89d6c44fe3dc8c3fedc0716580dc197da33145ef
brightfanta <brightfanta@gmail.com>
2023-12-03 23:15:26 +0900

    feat: 기능 구현 메서드 실행을 위해 메인 메서드에서 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameController.startGame();
+
+        GameController.repeatGame();
+//        Numbers numbers = GameController.requestNumbers();
+//        GameController.responseGameResult(numbers);
     }
 }
