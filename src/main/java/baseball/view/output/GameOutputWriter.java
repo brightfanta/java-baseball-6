f2f17986732189ac5e9fecf9c3debe9d9c7eeb6b
brightfanta <brightfanta@gmail.com>
2023-12-03 23:13:45 +0900

    feat: 비즈니스로직 연산 결과에 따른 출력메서드 구현

--- src/main/java/baseball/view/output/GameOutputWriter.java
+++ src/main/java/baseball/view/output/GameOutputWriter.java
@@ -1,6 +1,28 @@
 package baseball.view.output;
 
+import baseball.controller.dto.GameResponse;
+import baseball.view.constants.ResponseFormat;
+
+import static baseball.domain.Numbers.*;
+
 public final class GameOutputWriter extends OutputWriter{
     public GameOutputWriter() {
+    }
+
+    public static void printMatchingResult(GameResponse gameResponse) {
+        int strikeCount = gameResponse.strikeCount();
+        int ballCount = gameResponse.ballCount();
+        if (strikeCount != NOT_EXISTENCE && ballCount != NOT_EXISTENCE) {
+            println(ResponseFormat.BALL_AND_STRIKE.generateFormat(ballCount, strikeCount));
+        }
+        if (strikeCount != NOT_EXISTENCE && ballCount == NOT_EXISTENCE) {
+            println(ResponseFormat.STRIKE_ONLY.generateFormat(strikeCount));
+        }
+        if (strikeCount == NOT_EXISTENCE && ballCount != NOT_EXISTENCE) {
+            println(ResponseFormat.BALL_ONLY.generateFormat(ballCount));
+        }
+        if (strikeCount == NOT_EXISTENCE && ballCount == NOT_EXISTENCE) {
+            println(ResponseFormat.NOTHING.generateFormat(NOT_EXISTENCE));
+        }
     }
 }
