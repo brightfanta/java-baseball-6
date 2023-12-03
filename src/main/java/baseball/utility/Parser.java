856f43d78619908111a27abe58ba595a8a4e3bce
brightfanta <brightfanta@gmail.com>
2023-12-03 21:51:48 +0900

    feat: 입력 숫자 배열 변환 메서드 추가

--- src/main/java/baseball/utility/Parser.java
+++ src/main/java/baseball/utility/Parser.java
@@ -2,11 +2,32 @@ package baseball.utility;
 
 import baseball.exception.ExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Parser {
     private Parser() {
     }
 
     public static int parseNumberInput(String numberInput) {
         return ExceptionHandler.tryOnParseIntException(() -> Integer.parseInt(numberInput));
+    }
+
+    public static List<Integer> parseNumbers(int number) {
+        List<Integer> playerNumbers = new ArrayList<>();
+        int[] numbers = Stream.of(String.valueOf(number).split(""))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        Arrays.stream(numbers)
+                .forEach(playerNumbers::add);
+
+        return playerNumbers;
+    }
+
+    private static boolean hasWhitespace(String input) {
+        return input.chars().anyMatch(Character::isWhitespace);
     }
 }
